db5cd4e853d9011429945f5a579b5f78235e00c6
kinky-panda <31036979+kinky-panda@users.noreply.github.com>
2017-10-08 16:00:31 +0200

    Add files via upload

---
+++ Hangman/Hangman/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Resources;
+using System.Media;
+
+namespace Hangman
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+        SoundPlayer a;
+		public MainWindow()
+		{
+			InitializeComponent();
+			LoopSound();
+		}
+
+		private void LoopSound()
+		{
+			Uri soundfile = new Uri("pack://application:,,,/gamesound.wav");
+			StreamResourceInfo sound = Application.GetResourceStream(soundfile);
+			SoundPlayer a = new SoundPlayer(sound.Stream);
+			a.PlayLooping();
+		}
+
+		private void btnPlay_Click(object sender, RoutedEventArgs e)
+		{
+			string[] Names = {"2306Ren" } ;
+			int i;
+			for (i = 0; i < Names.Length; i++)
+			{
+				if (txtUsername.Text == Names[i])
+				{
+					this.Close();
+					new Window1().Show();
+				}
+			}
+			if(txtUsername.Text != Names[i])
+			{
+				MessageBox.Show("Please register to play");
+			}
+		}
+
+		private void btnRegister_Click(object sender, RoutedEventArgs e)
+		{
+			this.Close();
+			new Window2().Show();
+		}
+	}
+}
