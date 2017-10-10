ac3995079aa6993a232a0b39458abd91284a3456
lingling <kinkypanda2011@gmail.com>
2017-10-10 19:31:49 +0200

    review ReadMe for errors

---
+++ Hangman Final/Hangman/Hangman/Player.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangman
+{
+    class Player
+    {
+        public string Username { get; set; }
+        public int Score { get; set; }
+
+        public Player(string name, int score)
+        {
+            Username = name;
+            Score = score;
+        }
+
+        public Player(string name)
+        {
+            Username = name;
+            Score = 0;
+        }
+
+
+    }
+}
