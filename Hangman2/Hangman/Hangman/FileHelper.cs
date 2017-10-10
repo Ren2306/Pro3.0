6594a4940d253696cbd0e4abbd61270462af3575
lingling <kinkypanda2011@gmail.com>
2017-10-10 20:01:53 +0200

    final

---
+++ Hangman2/Hangman/Hangman/FileHelper.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Hangman
+{
+    class FileHelper
+    {
+        string category;
+        List<string> words;
+        public FileHelper(string file)
+        {
+            category = file+".txt";
+            words = new List<string>();
+            init();
+        }
+
+        private void init()
+        {
+            if (File.Exists(category))
+            {
+                StreamReader reader = File.OpenText(category);
+                string line = reader.ReadLine();
+
+                while (line != null)
+                {
+                    words.Add(line);
+                    line = reader.ReadLine();
+                }
+            }
+        }
+
+        public List<string> JumbledWords()
+        {
+            List<string> JumbledWords = new List<string>();
+            Random r = new Random();
+            
+            foreach (string word in words)
+            {
+                int size = word.Length - 1;
+                string jumbled_word = "";
+                for(int k=0; k<word.Length; k++)
+                {
+                    int idx = r.Next(0, size);
+                    if(idx!= word.IndexOf(' '))
+                    {
+                        jumbled_word += word[idx];
+                    }
+                }
+                JumbledWords.Add(jumbled_word);
+            }
+
+            return JumbledWords;
+        }
+    }
+}
---
+++ Hangman2/Hangman/Hangman/HangmanAvatar.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace Hangman
+{
+    class HangmanAvatar
+    {
+
+        string inThisProject = "pack://application:,,,/";
+
+        private BitmapImage[] images;
+
+        private Image Hangman;
+
+        public HangmanAvatar(Image hangman)
+        {
+            images = new BitmapImage[] {
+                    new BitmapImage(new Uri(inThisProject + "hanger.png")),
+                    new BitmapImage(new Uri(inThisProject + "hangman_stage0.png")),
+                    new BitmapImage(new Uri(inThisProject + "hangman_stage1.png")),
+                    new BitmapImage(new Uri(inThisProject + "hangman_stage2.png")),
+                    new BitmapImage(new Uri(inThisProject + "hangman_stage3.png")),
+                    new BitmapImage(new Uri(inThisProject + "hangman_stage4.png")),
+                    new BitmapImage(new Uri(inThisProject + "hangman_stage5.png")) };
+
+            Hangman = hangman;
+            Hangman.Source = images[0];
+
+        }
+
+        public void Reset()
+        {
+            Hangman.Source = images[0];
+        }
+
+        public void AddLimb(int state)
+        {
+            Hangman.Source = images[state];
+        }
+    }
+}
